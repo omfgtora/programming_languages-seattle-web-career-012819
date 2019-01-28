68d41260c45ec82994b008d8ab5135a2172e7f3a
Ethan Roberts <ejrobe@gmail.com>
2019-01-28 03:10:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  # new_hash = {}
+  # languages.each {|type, hash|
+  #   puts x
+  # }
+  # new_languages = {}
+  # languages.each do |type, hash|
+  #   hash.each do |name, attributes|
+  #     new_languages[name] ||= attributes
+  #     new_languages[name][:style] ||= []
+  #     new_languages[name][:style] << type
+  #   end
+  # end
+  # new_languages
+  
 end
