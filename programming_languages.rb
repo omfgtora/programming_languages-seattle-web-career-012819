91430db6ed27140ab2451064c42d4a76f3897ba5
Ethan Roberts <ejrobe@gmail.com>
2019-01-28 03:10:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,4 @@
 def reformat_languages(languages)
-  # new_hash = {}
-  # languages.each {|type, hash|
-  #   puts x
-  # }
   # new_languages = {}
   # languages.each do |type, hash|
   #   hash.each do |name, attributes|
